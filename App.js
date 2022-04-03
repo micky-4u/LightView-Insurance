81c9d1ce5bf0a5f9d0530280bffede2f6064f220
Sylvester Abiah <malonekeller@gmail.com>
2022-04-03 11:10:43 +0000

    Initial commit

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
+//import 'react-native-gesture-handler';
+
 import { StyleSheet, Text, View } from 'react-native';
 
+import { useFonts } from 'expo-font';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Welcome from "./screens/WelcomeScreen"
+import Signup from "./screens/SignupScreen"
+import Login from "./screens/LoginScreen"
+
+const Stack = createNativeStackNavigator()
+
 export default function App() {
+  const [loaded] = useFonts({
+    Semibold: require('./assets/fonts/Poppins-SemiBold.ttf'),
+    Medium: require('./assets/fonts/Poppins-Medium.ttf'),
+    Regular: require('./assets/fonts/Poppins-Regular.ttf'),
+    Light: require('./assets/fonts/Poppins-Light.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator 
+      screenOptions={{headerShown: false}}
+      initialRouteName="Welcome"
+      >
+        <Stack.Screen name="Welcome" component={Welcome}/>
+        <Stack.Screen name="Login" component={Login}/>
+        <Stack.Screen name="Signup" component={Signup}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
