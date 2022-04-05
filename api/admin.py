18256d7f564be6d798a324a0b86aaddd0d22e6a7
Mawuli Badassou <mawulibadassou5@gmail.com>
2022-04-05 22:12:23 +0000

    Added Agent model and modified client model

--- api/admin.py
+++ api/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Client
+from .models import Agent, Client
 
 admin.site.register(Client)
+admin.site.register(Agent)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,4 @@
+from datetime import timezone
 from django.db import models
 
 # Create your models here.
@@ -13,6 +14,24 @@ class Client(models.Model):
     details = models.CharField(max_length=300)
     address = models.CharField(max_length=200, blank=True, null=True)
     is_active = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.last_name
+
+
+class Agent(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    last_name = models.CharField(max_length=100)
+    other_name = models.CharField(max_length=100)
+    phone = models.IntegerField()
+    date_of_birth = models.DateField()
+    email = models.EmailField()
+    details = models.CharField(max_length=300)
+    address = models.CharField(max_length=200, blank=True, null=True)
+    is_active = models.BooleanField(default=False)
+    stat_date = models.DateField()
+    end_date = models.DateField()
 
     def __str__(self):
         return self.last_name
