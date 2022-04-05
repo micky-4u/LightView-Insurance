b627e2898ac92d406d3f7236828c2a8ac1e8e4d0
Mawuli Badassou <mawulibadassou5@gmail.com>
2022-04-05 19:56:19 +0000

    Added Client model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Client(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    last_name = models.CharField(max_length=100)
+    other_name = models.CharField(max_length=100)
+    phone = models.IntegerField()
+    date_of_birth = models.DateField()
+    email = models.EmailField()
+    details = models.CharField(max_length=300)
+    address = models.CharField(max_length=200, blank=True, null=True)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.last_name
