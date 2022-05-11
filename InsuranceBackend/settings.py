e62a8f04b1019ce408ac13d5f169d4c728bd92f2
micky-4u <laryeamichel4u@gmail.com>
2022-05-11 06:52:44 +0000

    hosting files

--- InsuranceBackend/settings.py
+++ InsuranceBackend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "v9i@kqi!8dyyp@kl)=!lly3=+!6mu7utb1t8b)5&=p_3j6&ca!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -129,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
--- frontend/components/Colors.js
+++
@@ -1,4 +0,0 @@
-export default {
-    main: "#0028B6",
-    white: "#F1F1F1"
-}
