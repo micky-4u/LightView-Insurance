8fe1d3f205b88afb119fedffd7af8dbef1a4c29c
micky-4u <laryeamichel4u@gmail.com>
2022-06-01 17:28:36 +0000

    made changes to some models

--- policies/views.py
+++ policies/views.py
@@ -1,6 +1,6 @@
 from multiprocessing import Pool
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from .models import PolicyType,Case, Offers
 from .serializers import PolicyTypeSerializer, CaseSerializer,OfferSerializer
@@ -10,16 +10,20 @@ from .serializers import PolicyTypeSerializer, CaseSerializer,OfferSerializer
 class PolicyTypeList(generics.ListCreateAPIView):
     queryset = PolicyType.objects.all()
     serializer_class = PolicyTypeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class PolicyTypeDetails(generics.RetrieveUpdateAPIView):
     queryset = PolicyType.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = PolicyTypeSerializer
     
 class CaseList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Case.objects.all()
     serializer_class = CaseSerializer
 
 class CaseDetails(generics.RetrieveUpdateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Case.objects.all()
     serializer_class = CaseSerializer
     
@@ -27,10 +31,14 @@ class CaseDetails(generics.RetrieveUpdateAPIView):
 class OfferList(generics.ListCreateAPIView):
     queryset = Offers.objects.all()
     serializer_class = OfferSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    
     
 class OfferDetails(generics.RetrieveUpdateAPIView):
     queryset = Offers.objects.all()
     serializer_class = OfferSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    
     
     
 
