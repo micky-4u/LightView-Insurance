056e514d69b7e7a345f62cf903b2a7d055af0c76
micky-4u <laryeamichel4u@gmail.com>
2022-06-01 18:17:55 +0000

    made changes to some models

--- client/urls.py
+++ client/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from django.contrib import admin
-from .views import ClientDetails, ClientList, ClientAdd, ClientEdit
+from .views import ClientDetails, ClientList, ClientAdd, ClientEdit, ClientRm
 
 
 urlpatterns = [
@@ -8,6 +8,6 @@ urlpatterns = [
     path("<int:pk>", ClientDetails.as_view()),
     path("add", ClientAdd.as_view()),
     path("edit/<int:pk>", ClientEdit.as_view()),
-    path("rm/<int:pk>", ClientEdit.as_view()),
+    path("rm/<int:pk>", ClientRm.as_view()),
 ]
 
