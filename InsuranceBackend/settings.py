a63e0341f10e3005bf7d9bf4b4c3a0d13f30079e
micky-4u <laryeamichel4u@gmail.com>
2022-06-01 10:04:24 +0000

    made changes to some models

--- InsuranceBackend/settings.py
+++ InsuranceBackend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "v9i@kqi!8dyyp@kl)=!lly3=+!6mu7utb1t8b)5&=p_3j6&ca!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost","lightviewinc.herokuapp.com"]
 
 
 # Application definition
@@ -138,7 +138,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         # 'rest_framework.authentication.BasicAuthentication',
         # 'rest_framework.authentication.SessionAuthentication',
-        'knox.auth.TokenAuthentication',
+        'knox.auth.TokenAuthentication'
         
     )
 }
