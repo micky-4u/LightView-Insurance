334261a7342e454426d10d10b2452e394f0dacfa
micky-4u <laryeamichel4u@gmail.com>
2022-06-02 14:42:32 +0000

    additional files

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ urlpatterns = [
     path("agents/", include("agent.urls")),
     path("", include("administrator.urls")),
     path("clients/", include("client.urls")),
-    path("/", include("policies.urls")),
+    path("", include("policies.urls")),
     path("accounts/", include("accounts.urls")),
     path("products/type/life/", include("lifeInsurance.urls")),
     path("products/type/health/", include("healthInsurance.urls")),
