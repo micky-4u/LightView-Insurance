bbd2289bf30b87dfe8c5766dbd17c71c42ad1cab
micky-4u <laryeamichel4u@gmail.com>
2022-06-02 14:54:34 +0000

    additional files

--- policies/urls.py
+++ policies/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     path("policy/cases", CaseList.as_view()),
     path("policy/cases/<int:pk>", CaseDetails.as_view()),
     path("offers", OfferList.as_view()),
-    path("offers/<int:pk>", OfferDetails.as_view()),
+    path("offers/<int:offer_code>", OfferDetails.as_view()),
     path("offers/add", OfferAdd.as_view()),
-    path("offers/rm", OfferRm.as_view()),
+    path("offers/rm/<int:offer_code>", OfferRm.as_view()),
     
 ]
 
