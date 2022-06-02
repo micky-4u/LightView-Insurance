0d4ad3c9abf75ca42886c2eeacf59c13a2f1612c
micky-4u <laryeamichel4u@gmail.com>
2022-06-02 06:08:10 +0000

    additional files

--- agent/models.py
+++ agent/models.py
@@ -8,7 +8,7 @@ codes = uuid.uuid1()
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete= models.CASCADE)
     phone = models.IntegerField()
-    agent_code = models.IntegerField(max_length=100, blank=True, default= codes.time, auto_created=True)
+    agent_code = models.IntegerField(blank=True, default= codes.time, auto_created=True)
     date_of_birth = models.DateField()
     nationality = models.CharField(max_length=100)
     national_id = models.CharField(max_length=100)
