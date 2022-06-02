c2d9f75e90938e39337103584ce90eb92e23ea9f
micky-4u <laryeamichel4u@gmail.com>
2022-06-02 13:54:09 +0000

    additional files

--- administrator/urls.py
+++ administrator/urls.py
@@ -6,7 +6,7 @@ from .views import UserDetail, UserList, UserRm,RegisterAPI
 urlpatterns = [
     path("users", UserList.as_view()),
     path("admins/register", RegisterAPI.as_view()),
-    path("<int:pk>", UserDetail.as_view()),
-    path("rm/<int:pk>", UserRm.as_view()),
+    path("users/<int:pk>", UserDetail.as_view()),
+    path("users/rm/<int:pk>", UserRm.as_view()),
 ]
 
